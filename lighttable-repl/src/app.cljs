e5567bac9fb216c50b18b188cea3a477647b44cd
popmilo <popmilo2005@gmail.com>
2013-10-05 10:34:50 +0200

    First working draft with canvas element.

--- lighttable-repl/src/app.cljs
+++ lighttable-repl/src/app.cljs
@@ -1,10 +1,21 @@
 (ns app
     (:require [app.world :as world]))
 
-(js/alert (world/default-string))
+;(js/alert (world/default-string))
 
-(defn hack-body [] (set! (.. js/document -body -innerHTML) "<h1>This has been hacked </h1>"))
+(defn drawSquare []
+    (let [target (.getElementById js/document "tunelcanvas")
+          context (.getContext target "2d")]
+       (.fillRect context 0 0 100 100)
+    ))
 
-;umcoment line below and command/control enter it to evaluate it
+(defn ^:export init []
+     (drawSquare)
+  )
+
+(defn hack-body [] (set! (.. js/document -body -innerHTML) "<h1>Vidi жџћчћжђжџ ! </h1> <h1>Nézd meg' áűéüóÜ ! </h1>"))
+(init)
+;(init)
+;uncoment line below and command/control enter it to evaluate it
 
 ;(hack-body)
